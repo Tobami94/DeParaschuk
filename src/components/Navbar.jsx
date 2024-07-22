7a6fc11e7b46584dd335fb99f634dafdf2593dc1
unknown <malenaparaschuk@gmail.com>
2024-07-22 23:42:58 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef, forwardRef } from "react";
 import { useScrollPosition } from "../hooks/useScrollPosition";
 import useResizeObserver from "../hooks/useResizeObserver";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import {
-  about,
-  experience,
-  proyects,
-} from "../editable-stuff/config.js";
+import { about, experience, proyects } from "../editable-stuff/config.js";
 import { NavLink } from "./home/migration";
 
-const Navigation = React.forwardRef((props, ref) => {
+const Navigation = forwardRef((props, ref) => {
   const [isTop, setIsTop] = useState(true);
   const [scrollPosition, setScrollPosition] = useState(0);
-  const navbarMenuRef = React.useRef();
+  const navbarMenuRef = useRef();
   const navbarDimensions = useResizeObserver(navbarMenuRef);
   const navBottom = navbarDimensions ? navbarDimensions.bottom : 0;
+
   useScrollPosition(
     ({ prevPos, currPos }) => {
-      if (!navbarDimensions) return;
+      if (!navbarDimensions || !ref || !ref.current) return;
       currPos.y + ref.current.offsetTop - navbarDimensions.bottom > 5
         ? setIsTop(true)
         : setIsTop(false);
       setScrollPosition(currPos.y);
     },
-    [navBottom]
+    [navBottom, ref, navbarDimensions]
   );
 
-  React.useEffect(() => {
-    if (!navbarDimensions) return;
+  useEffect(() => {
+    if (!navbarDimensions || !ref || !ref.current) return;
     navBottom - scrollPosition >= ref.current.offsetTop
       ? setIsTop(false)
       : setIsTop(true);
@@ -37,30 +34,24 @@ const Navigation = React.forwardRef((props, ref) => {
   return (
     <Navbar
       ref={navbarMenuRef}
-      className={`px-3 fixed-top  ${
-        !isTop ? "navbar-white" : "navbar-transparent"
-      }`}
-      expand="lg">
+      className={`px-3 fixed-top  ${!isTop ? "navbar-white" : "navbar-transparent"}`}
+      expand="lg"
+    >
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggler" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="navbar-nav mr-auto">
           {about.show && (
-            <NavLink
-              className="nav-item lead"
-              href={process.env.PUBLIC_URL + "/#aboutme"}>
+            <NavLink className="nav-item lead" href={process.env.PUBLIC_URL + "/#aboutme"}>
               ¿Quién soy?
             </NavLink>
           )}
           {experience.show && (
-            <NavLink
-              className="nav-item lead"
-              href={process.env.PUBLIC_URL + "/#skills"}>
+            <NavLink className="nav-item lead" href={process.env.PUBLIC_URL + "/#skills"}>
               Experiencia
             </NavLink>
           )}
-
           {proyects.show && (
-            <NavLink href={process.env.PUBLIC_URL + "/#projects"}>
+            <NavLink className="nav-item lead" href={process.env.PUBLIC_URL + "/#projects"}>
               Proyectos
             </NavLink>
           )}
